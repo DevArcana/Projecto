0e3ff519c6e61752c1d7cefff212a1d8957bd7b8
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-08-31 15:34:01 +0200

    add error404

--- src/Projecto/ClientApp/src/app/app-routing.module.ts
+++ src/Projecto/ClientApp/src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
+import { Error404Component } from './error404/error404.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) }];
-
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'projects'
+  },
+  {
+    path: 'projects',
+    loadChildren: () => import('./projects/projects.module').then((m) => m.ProjectsModule),
+  },
+  {
+    path: '**',
+    component: Error404Component
+  }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/Projecto/ClientApp/src/app/app.module.ts
+++ src/Projecto/ClientApp/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './layout/layout.module';
+import { LoginComponent } from './auth/login/login.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
