5764f6a640bcd8f37d3dbae22be214fedc59e8f8
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2020-09-15 17:30:02 +0200

    scaffold project creation form

--- src/projecto-client-angular/src/app/main/projects/projects.module.ts
+++ src/projecto-client-angular/src/app/main/projects/projects.module.ts
@@ -6,13 +6,16 @@ import { ProjectsComponent } from './projects.component';
 import { ProjectsListComponent } from './projects-list/projects-list.component';
 import { ProjectComponent } from './projects-list/project/project.component';
 import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { CreateProjectFormComponent } from './create-project-form/create-project-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [ProjectsComponent, ProjectsListComponent, ProjectComponent, ProjectDetailsComponent],
+  declarations: [ProjectsComponent, ProjectsListComponent, ProjectComponent, ProjectDetailsComponent, CreateProjectFormComponent],
   imports: [
     CommonModule,
-    ProjectsRoutingModule
+    ProjectsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ProjectsModule { }
